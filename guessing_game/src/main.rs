bedeb68bb4a53f9a5a9ebb24b14af8169c032b75
Mateus Preste <mateusx.mk@gmail.com>
2023-01-20 17:29:36 -0400

    add rand crate to project

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,23 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
-    let mut guess : String = String::new(); // muttable variable that contains the guess value
+    let mut guess : String = String::new(); 
 
-    let result = io::stdin()
-        .read_line(&mut guess)
+    let size = io::stdin()
+        .read_line(&mut guess) // read_line APPEND the input to the string
         .expect("msg");
 
-    println!("You guess is {guess} and the size {result}");
+    let guess: i32 = guess.trim().parse().expect("expecting a integer");
+
+    println!("You guess is {guess} and the size {:?}", size);
+
 }
